751c3b81543a59bfa0967dfa8a2cd41c68d85482
LSV310 <anicet.gruet@gmail.com>
2025-07-23 18:04:44 +0200

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int	main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << &string << " | " << stringPTR << " | " << &stringREF << std::endl;
+	std::cout << string << " | " << *stringPTR << " | " << stringREF << std::endl;
+}
