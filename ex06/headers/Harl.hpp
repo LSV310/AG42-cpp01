af3ff41841c71794f2828ed990355c31147eff3d
LSV310 <anicet.gruet@gmail.com>
2025-07-25 17:53:05 +0200

    ongoing ex06

---
+++ ex06/headers/Harl.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+
+using std::string;
+
+class Harl {
+
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		Harl(void);
+		~Harl(void);
+
+		void	complain(std::string level);
+
+};
